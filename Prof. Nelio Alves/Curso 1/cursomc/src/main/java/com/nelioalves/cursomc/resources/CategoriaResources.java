b764f3a0de67690202fb1f756c22e838a6c7d304
rvmodesto <raquel.modesto2@gmail.com>
2022-03-01 15:00:37 -0300

    Paginação com parametros opcionais na requisição

--- Prof. Nelio Alves/Curso 1/cursomc/src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
+++ Prof. Nelio Alves/Curso 1/cursomc/src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
@@ -1,17 +1,22 @@
 package com.nelioalves.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
@@ -21,6 +26,15 @@ public class CategoriaResources {
 	@Autowired
 	private CategoriaService categoriaService;
 
+	
+	//DTO - ESPECIFICAR O QUE SERÁ EXIBIDO
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity <List<CategoriaDTO>> findAll() {
+		List<Categoria> list = categoriaService.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = categoriaService.findById(id);
@@ -48,4 +62,17 @@ public class CategoriaResources {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/page",method=RequestMethod.GET)
+	public ResponseEntity <Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, //24 = multi de 2, facilita na responsividade 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {  //ASC - ascendente/DESC
+		Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 }
--- Prof. Nelio Alves/Curso 1/cursomc/src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ Prof. Nelio Alves/Curso 1/cursomc/src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -17,6 +21,10 @@ public class CategoriaService {
 	//autowired = injeção de dependencia
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
+	}
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
@@ -42,5 +50,10 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityExceptions("Não é possível excluir uma categoria que possui produtos!!");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 }
